2c63076ffc7a2e2778956df84b8b38fc5245f175
khoi_P <huynhvankhoi97@gmail.com>
2025-06-06 17:29:17 +0700

    xong section 15 (routing) nhưng chưa xong section 14 (search book)

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,20 @@ import "./App.css";
 import { Navbar } from "./layout/NavbarAndFooter/Navbar";
 import { Footer } from "./layout/NavbarAndFooter/Footer";
 import { HomePage } from "./layout/HomePage/HomePage";
+import { SearchBooksPage } from "./layout/SearchBooksPage/SearchBooksPage";
+import { Routes, Route } from "react-router-dom";
 
 export const App = () => {
   return (
-    <div>
+    <div className="d-flex flex-column min-vh-100">
       <Navbar />
-      <HomePage />
+      <div className="flex-grow-1">
+        <Routes>
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/search" element={<SearchBooksPage />} />
+        </Routes>
+      </div>
       <Footer />
     </div>
   );
