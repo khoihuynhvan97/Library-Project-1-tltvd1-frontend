5cc5d32a991e860724a1fd24172c5f1c681001b3
khoi_P <huynhvankhoi97@gmail.com>
2025-06-18 16:04:23 +0700

    Xong Section 16, copy code giống 100%, code chạy bình thường và không có lỗi

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import { Navbar } from "./layout/NavbarAndFooter/Navbar";
 import { Footer } from "./layout/NavbarAndFooter/Footer";
 import { HomePage } from "./layout/HomePage/HomePage";
 import { SearchBooksPage } from "./layout/SearchBooksPage/SearchBooksPage";
-import { Routes, Route } from "react-router-dom";
-
+import { BookCheckoutPage } from "./layout/BookCheckoutPage/BookCheckoutPage";
 export const App = () => {
   return (
     <div className="d-flex flex-column min-vh-100">
@@ -15,6 +15,8 @@ export const App = () => {
           <Route path="/home" element={<HomePage />} />
           <Route path="/" element={<HomePage />} />
           <Route path="/search" element={<SearchBooksPage />} />
+          <Route path="/checkout/:bookId" element={<BookCheckoutPage />} />
+
         </Routes>
       </div>
       <Footer />
